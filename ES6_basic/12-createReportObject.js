a2625faa8a21cc3e1c851f28bc9e08882df4ee82
meisibley <mkong99@yahoo.com>
2024-01-04 15:48:14 -0600

    12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments(employeesList) {
-      return Object.Keys(employeesList).length;
+    getNumberOfDepartments() {
+      return Object.Keys(this.allEmployees).length;
     },
   };
 }
