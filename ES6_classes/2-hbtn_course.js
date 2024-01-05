c25f06af3e981590dc8aead9fc5c798bc1dcc53a
meisibley <mkong99@yahoo.com>
2024-01-05 11:42:11 -0600

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,21 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
     this._name = name;
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
     this._length = length;
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    } else {
+      students.forEach((eachS) => {
+        if (typeof eachS !== 'string') {
+          throw TypeError('Students must be an array of strings');
+        }
+    }
     this._students = students;
   }
 
@@ -29,7 +43,7 @@ export default class HolbertonCourse {
 
   set students(inputStudents) {
     if (!Array.isArray(inputStudents)) {
-       throw TypeError('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     } else {
       inputStudents.forEach((eachStudent) => {
         if (typeof eachStudent !== 'string') {
@@ -42,5 +56,5 @@ export default class HolbertonCourse {
 
   get students() {
     return this._students;
-  }  
+  }
 }
