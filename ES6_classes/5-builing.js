27a9a501790d1c4b89247617ba8af3446efadec8
meisibley <mkong99@yahoo.com>
2024-01-05 16:36:41 -0600

    5-builing.js

---
+++ ES6_classes/5-builing.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    if (!this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  set sqft(inputSqft) {
+    if (typeof inputSqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    this._sqft = inputSqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
