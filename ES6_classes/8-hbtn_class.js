3729af59c620d29a802b43c780b210cfae30084c
meisibley <mkong99@yahoo.com>
2024-01-05 20:05:01 -0600

    8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,20 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    this._size = size;
+    if (typeof location !== 'string') {
+      throw TypeError('Location must be a string');
+    }
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](inputValue) {
+    if (inputValue === 'number') {
+      return this._size;
+    } else {
+      return this._location;
+    }
+  }
+}
