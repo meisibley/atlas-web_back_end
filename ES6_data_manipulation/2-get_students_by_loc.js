4fd12385d675d48310072b552f1730c3bba66ade
meisibley <mkong99@yahoo.com>
2024-01-08 16:36:21 -0600

    2-get_students_by_loc.js

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(studentsArray, city) {
+  if (!Array.isArray(studentsArray)) {
+    return [];
+  }
+  const cityArray = studentsArray.filter((item) => item[2] === city); 
+  return cityArray;
+}
