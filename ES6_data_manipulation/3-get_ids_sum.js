7e4ad0395214a7a4c1653e13fea6a933e5efb33f
meisibley <mkong99@yahoo.com>
2024-01-08 17:28:37 -0600

    3-get_ids_sum.js

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -3,7 +3,7 @@ import getListStudentIds from './1-get_list_student_ids';
 export default function getStudentIdsSum(students) {
   const idList = getListStudentIds(students);
   const sum = idList.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
+    (accumulator, currentValue) => accumulator + currentValue,
   );
   return sum;
 }
