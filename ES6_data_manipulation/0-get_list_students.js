3512326d25b5eb5667c2ba76234f10deb777e3dd
meisibley <mkong99@yahoo.com>
2024-01-08 13:10:16 -0600

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
-  obj1 = {
+  const obj1 = {
     firstName: 'Guillaume',
     id: 1,
     location: 'San Francisco',
   };
-  obj2 = {
+  const obj2 = {
     firstName: 'James',
     id: 2,
     location: 'Columbia',
   }
-  obj3 = {
+  const obj3 = {
     firstName: 'Serena',
     id: 5,
     location: 'San Francisco',
   }
-  arr = [obj1, obj2, obj3];
+  const arr = [obj1, obj2, obj3];
   return arr;
 }
