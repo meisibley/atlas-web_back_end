ca45c871f1327e546cda492670df25f62f91a54b
meisibley <mkong99@yahoo.com>
2024-01-08 19:42:20 -0600

    4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsLocat = students.filter((item) => item.location === city);
-  
+
   // initial all students of the city grades to N/A
   for (const elem of studentsLocat) {
     elem.grade = 'N/A';
   }
   const studentsUpdateNewGrades = studentsLocat.map((item) => {
-    for (const nGrade of newGrade) {
+    for (const nGrade of newGrades) {
       // if the student's id is in newGrade, update it's grade
       if (item.id === nGrade.studentId) {
         item.grade = nGrade.grade;
