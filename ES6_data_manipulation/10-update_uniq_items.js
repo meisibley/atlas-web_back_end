b9b381082ec053403e455b861b27aa8c2095c06b
meisibley <mkong99@yahoo.com>
2024-01-09 10:43:31 -0600

    10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,10 @@
-export default function updateUniqueItems(map) {
-  if (!Map.isMap(map)) {
+export default function updateUniqueItems(groceryMap) {
+  if (!Map.isMap(groceryMap)) {
     throw new Error('Cannot process');
   }
 
-  for (const [name, quantity] of map.entries()) {
-    if (quantity === 1) {
-      map.set(name, 100);
-    }
+  for (const [name, quantity] of groceryMap.entries()) {
+    if (quantity === 1) groceryMap.set(name, 100);
   }
-  return map;
+  return groceryMap;
 }
