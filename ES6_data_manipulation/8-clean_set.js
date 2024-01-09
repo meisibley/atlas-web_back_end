ef3f4bcc6d5bb4fdd30b16cc4b6c7d32c7214019
meisibley <mkong99@yahoo.com>
2024-01-08 23:35:51 -0600

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString === '') {
     return '';
   }
-  return [...set]
+  return [...set].filter((element) => element)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
