275e170c95a52cc864338e2bd7510b8c1c06a0d5
meisibley <mkong99@yahoo.com>
2024-01-10 14:58:45 -0600

    1-promise.js

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function gerFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
     }
