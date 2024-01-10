d7ea84b271ec0291e40824f35232f696f2c70990
meisibley <mkong99@yahoo.com>
2024-01-10 15:20:59 -0600

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-export default handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
     console.log('Got a response from the API');
     return ({
