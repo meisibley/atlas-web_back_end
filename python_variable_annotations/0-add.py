fd15da6157b3a5b37ff65acfc3da72ce79ace984
meisibley <mkong99@yahoo.com>
2024-04-29 10:29:55 -0500

    task0 - Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# function add takes float a and float b and returns their sum float.
+'''function add takes float a and float b and returns their sum float.'''
 
 
 def add(a: float, b: float) -> float:
-    # function add takes float a and float b and returns their sum float.
+    '''function add takes float a and float b and returns their sum float.'''
     return a + b
