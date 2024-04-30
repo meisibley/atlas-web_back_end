1e43f93cc2be7d60fb3ca6fa018ed92eee50b4d7
meisibley <mkong99@yahoo.com>
2024-04-30 09:18:16 -0500

    task4 - Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''Define and annotate the following variables with the specified values:
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”'''
+
+
+'''a, an integer with a value of 1'''
+a: int = 1
+
+'''pi, a float with a value of 3.14'''
+pi: float = 3.14
+
+'''i_understand_annotations, a boolean with a value of True'''
+i_understand_annotations: bool = True
+
+'''school, a string with a value of “Holberton”'''
+school: str = "Holberton"
