c8c1b94169f34023a5b8ebf0c68c7b8a99729f3d
meisibley <mkong99@yahoo.com>
2024-05-13 18:26:52 +0000

    task2 - Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -50,3 +50,24 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super().format(record)
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+""" Create a tuple PII_FIELDS constant at the root of the module containing
+the fields from user_data.csv that are considered PII. PII_FIELDS can contain
+only 5 fields"""
+
+def get_logger() -> logging.Logger:
+    """ Implement a get_logger function that takes no arguments and
+    returns a logging.Logger object.
+    The logger should be named "user_data" and only log up to logging.INFO
+    level. It should not propagate messages to other loggers. It should
+    have a StreamHandler with RedactingFormatter as formatter.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
