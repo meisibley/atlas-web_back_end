534622d9200814d55bb2a75d701d3e4d8aa93e39
meisibley <mkong99@yahoo.com>
2024-05-17 21:36:56 +0000

    task5 - encrypting passwords

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,7 @@
 name password and returns a salted, hashed password, which is a byte string.
 Use the bcrypt package to perform the hashing (with hashpw).
 """
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
