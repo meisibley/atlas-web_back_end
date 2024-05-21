0886afd6f549bcac83866fe2e1ec179b652499e3
meisibley <mkong99@yahoo.com>
2024-05-21 15:43:09 +0000

    task8 - basic base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ class BasicAuth inherits from Auth """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
     """ BasicAuth inherits from Auth """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """ method extract_base64_authorization_header in this class returns
+        """ method extract_base64_authorization_header returns
         Base64 part of the Authorization header for a Basic Authentication:
         Return None if authorization_header is None
         Return None if authorization_header is not a string
@@ -22,3 +23,25 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, 
+                                           base64_authorization_header:
+                                               str) -> str:
+        """ returns decoded value of a Base64 str base64_authorization_header:
+        Return None if base64_authorization_header is None
+        Return None if base64_authorization_header is not a string
+        Return None if base64_authorization_header is not a valid Base64 
+        - you can use try/except
+        Otherwise, return the decoded value as UTF8 string 
+        - you can use decode('utf-8')"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base_encode = base64_authorization_header.encode('utf-8')
+            base_decode = b64decode(base_encode)
+            decode_value = base_decode.decode('utf-8')
+            return decode_value
+        except Exception:
+            return None
