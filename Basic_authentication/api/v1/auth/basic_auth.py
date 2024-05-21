2835cf1ad9d1b7c15c9d3fb63bdfc5ded6a54e65
meisibley <mkong99@yahoo.com>
2024-05-21 14:41:49 +0000

    task7 - basic base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,17 +4,17 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ BasicAuth inherits from Auth 
-    method extract_base64_authorization_header in this class returns Base64
-    part of the Authorization header for a Basic Authentication:
-    Return None if authorization_header is None
-    Return None if authorization_header is not a string
-    Return None if authorization_header doesn’t start by Basic
-    (with a space at the end)
-    Otherwise, return the value after Basic (after the space)
-    You can assume authorization_header contains only one Basic """
+    """ BasicAuth inherits from Auth """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """ method extract_base64_authorization_header in this class returns
+        Base64 part of the Authorization header for a Basic Authentication:
+        Return None if authorization_header is None
+        Return None if authorization_header is not a string
+        Return None if authorization_header doesn’t start by Basic
+        (with a space at the end)
+        Otherwise, return the value after Basic (after the space)
+        You can assume authorization_header contains only one Basic """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
