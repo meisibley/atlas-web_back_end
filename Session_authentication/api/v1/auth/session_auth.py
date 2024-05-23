fc63e01aac835c6f45297370dcb5910d9756eebb
meisibley <mkong99@yahoo.com>
2024-05-23 23:31:27 +0000

    task2 - create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,7 +10,7 @@ class SessionAuth(Auth):
     validate if everything inherits correctly without any overloading
     validate the “switch” by using environment variables"""
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id:
         Return None if user_id is None
@@ -29,4 +29,3 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-        
