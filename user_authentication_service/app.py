daf54c90b3e9012122f8fc9fc5e80b4f8080166a
meisibley <mkong99@yahoo.com>
2024-05-24 22:18:12 +0000

    task17 - get reset password token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -77,5 +77,21 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ The request is expected to contain form data with the "email" field.
+    If the email is not registered, respond with a 403 status code. 
+    Otherwise, generate a token and respond with a 200 HTTP status and follow
+    JSON payload:{"email": "<user email>", "reset_token": "<reset token>"}"""
+    email = request.form.get("email")
+    if email is None:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
