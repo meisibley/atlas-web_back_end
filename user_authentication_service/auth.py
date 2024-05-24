78bb9d07880ec268be170e3f071a3487e7e4cad5
meisibley <mkong99@yahoo.com>
2024-05-24 21:29:21 +0000

    task12 - find user by session id

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -62,3 +62,14 @@ class Auth:
         session_id = _generate_uuid()
         user.session_id = session_id
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ If the session ID is None or no user is found, return None.
+        Otherwise return the corresponding user. """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
