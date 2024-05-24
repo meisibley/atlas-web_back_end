261e5a02b8e891ee0c1c26766b40f28c214f9a04
meisibley <mkong99@yahoo.com>
2024-05-24 22:38:14 +0000

    task19 - update password end-point

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -92,6 +92,7 @@ def get_reset_password_token():
     except ValueError:
         abort(403)
 
+
 @app.route("/reset_password", methods=["PUT"])
 def update_password():
     """ The request is expected to contain form data with fields "email",
