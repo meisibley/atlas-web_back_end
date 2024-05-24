97197b2ed8e6cf153c29fd45ba353c80909f54fc
meisibley <mkong99@yahoo.com>
2024-05-24 00:50:45 +0000

    task5 - before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -67,8 +67,8 @@ def before_request() -> str:
     request.current_user = auth.current_user(request)
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if (auth.authorization_header(request) is None and
-        auth.session_cookie(request) is None):
+    if (auth.authorization_header(request) is None
+       and auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
