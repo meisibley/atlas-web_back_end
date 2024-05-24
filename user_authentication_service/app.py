1854fa6994876396717ba39f5b29daba349e1b33
meisibley <mkong99@yahoo.com>
2024-05-24 21:57:53 +0000

    task15 - user profile

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -63,5 +63,19 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """ The request is expected to contain a session_id cookie. Use it to
+    find the user. If the user exist, respond with a 200 HTTP status and the
+    following JSON payload {"email": "<user email>"}
+    If the session ID is invalid or the user does not exist, respond with a
+    403 HTTP status"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or session_id is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
