0a229a9e5e3706238e431881d49c580822a0c859
meisibley <mkong99@yahoo.com>
2024-05-24 21:20:39 +0000

    task11 - log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ a Flask app """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -31,6 +31,23 @@ def users():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """ The request is expected to contain form data with 'email' and a
+    'password' fields. If the login information is incorrect, use flask.abort
+    to respond with a 401 HTTP status. Otherwise, create a new session for
+    the user, store it the session ID as a cookie with key "session_id" on
+    the response and return a JSON payload of the form"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password or not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
