310855945f1c0e11fd8a356d4f1bdac7de414e87
meisibley <mkong99@yahoo.com>
2024-05-29 18:05:21 +0000

    task4 - test client parameterize and patch

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """ declare the TestGithubOrgClient class and implement test_org method """
+import unittest
 from unittest import mock
 from client import GithubOrgClient
 from unittest.mock import patch
 from parameterized import parameterized
 
 
-class TestGithubOrgClient(GithubOrgClient):
-    """ child class to test GithubOrgClient class methods """
-    @patch("client.get_json", return_value={"payload": True})
+class TestGithubOrgClient(unittest.TestCase):
+    """ child class of unittest.TestCase class methods """
     @parameterized.expand([
         ("google"),
         ("abc"),
     ])
-    def test_org(self, org, mock_get_json):
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name, mock_get_json):
         """ test GithubOrgClient.org and returns the correct value """
-        test_client = GithubOrgClient(org)
+        test_client = GithubOrgClient(org_name)
         test_return = test_client.org
         self.assertEqual(test_return, mock_get_json.return_value)
+        mock_get_json.assert_called_once
