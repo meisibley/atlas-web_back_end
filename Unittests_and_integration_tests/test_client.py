14f43ad55eda51c77fcccd61e7d8bb6d9a9021eb
meisibley <mkong99@yahoo.com>
2024-05-29 19:00:51 +0000

    task6 - client more patch

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -31,3 +31,16 @@ class TestGithubOrgClient(unittest.TestCase):
             test_client = GithubOrgClient(test_json.get("repos_url"))
             test_return = test_client._public_repos_url
             self.assertEqual(test_return, mock_org.return_value.get("repos_url"))
+
+    @patch("client.get_json", return_value=[{"name": "atlas"}])
+    def test_public_repos(self, mock_get_json):
+        """ unit-test GithubOrgClient.public_repos method """
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_callable=PropertyMock,
+                          return_value={"https://api.github.com/"}) as mock_val:
+            test_client = GithubOrgClient("atlas")
+            test_return = test_client.public_repos()
+            self.assertEqual(test_return, ["atlas"])
+            mock_get_json.assert_called_once
+            mock_val.assert_called_once
