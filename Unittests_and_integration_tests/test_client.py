1b27831a3649d73ac01e241df90a5971cccefc45
meisibley <mkong99@yahoo.com>
2024-05-29 19:21:11 +0000

    task7 - parameterize test has license

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_return, ["atlas"])
             mock_get_json.assert_called_once
             mock_v.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """ to unit-test GithudOrgClient.has_license """
+        test_client = GithubOrgClient("atlas")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(test_return, expected_result)
