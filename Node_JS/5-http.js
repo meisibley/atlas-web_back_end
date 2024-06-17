219014b3e41d9637e6865574f4691d5c22dd7e74
meisibley <mkong99@yahoo.com>
2024-06-17 12:58:32 -0500

    task5 - create Node HTTP module server call task3

--- Node_JS/5-http.js
+++ Node_JS/5-http.js
@@ -7,29 +7,24 @@ When the URL path is /students, it should display This is the list of our studen
   followed by the same content as the file 3-read_file_async.js (with and without the database)
   - the name of the database must be passed as argument of the file
 CSV file can contain empty lines (at the end) - and they are not a valid student!*/
-var http = require('http');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(function (req, res) {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-            res.write('Hello Holberton School!');
-            res.end();
-        } else if (req.url === '/students') {
-            await countStudents(process.argv[2])
-              .then((fileData) => {
-                res.write('This is the list of our students\n');
-                res.write(`Number of students: ${fileData.CS.num + fileData.SWE.num}\n`);
-                res.write(`Number of students in CS: ${fileData.CS.num}. List: ${fileData.CS.list}\n`);
-                res.write(`Number of students in SWE: ${fileData.SWE.num}. List: ${fileData.SWE.list}`);
-                res.end();
-            })
-            .catch((err) => {
-                res.write('This is the list of our students\n');
-                res.end(err.message);
-            });
-        }
-    }
-}).listen(1245);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/students') {
+    countStudents(process.argv[2], 0)
+      .then((success) => {
+        const outPut = `This is the list of our students\n${success}`;
+        res.end(outPut);
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
 
 module.exports = app;
