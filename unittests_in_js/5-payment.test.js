72c6f8d79aec5e99757317320c4ec7eb1bdba281
meisibley <mkong99@yahoo.com>
2024-06-22 15:19:14 -0500

    task5

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,29 +1,28 @@
 const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
 const sinon = require('sinon');
-const { expect } = require('chai');
 
 
-describe('Test suite', function () {
-	let consoleSpy;
+describe('test sendPaymentRequestToApi', function () {
+	let spy;
 
 	beforeEach(function () {
-		consoleSpy = sinon.consoleSpy(console, 'log');
+		spy = sinon.spy(console, 'log');
 	});
 
 	afterEach(function () {
-		consoleSpy.restore();
+		spy.restore();
 	});
 
 	it('validate response from console.log for value (100, 20)', function () {
 		sendPaymentRequestToApi(100, 20);
-		expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
-		expect(consoleSpy.calledOnce).to.be.true;
+		sinon.assert.calledOnce(spy);
+		sinon.assert.calledWithExactly(spy, 'The total is: 120');
 	});
 
 	it('validate response from console.log for value (10, 10)', function () {
 		sendPaymentRequestToApi(10, 10);
-		expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
-		expect(consoleSpy.calledOnce).to.be.true;
+		sinon.assert.calledOnce(spy);
+		sinon.assert.calledWithExactly(spy, 'The total is: 20');
 	});
-})
+});
