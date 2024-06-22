954307e9b08f579f6a531f0c64914626e5db044c
meisibley <mkong99@yahoo.com>
2024-06-22 15:41:24 -0500

    task6

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', (done) => {
+    it('async test with done', () => getPaymentTokenFromAPI(true).then((success) => {
+        chai.expect(success).to.eql({data: 'Successful response from the API'});
+    }));
+});
