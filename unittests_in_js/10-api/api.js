6796351e42a1891b015e9b61dfa3fade5e94c09a
meisibley <mkong99@yahoo.com>
2024-06-22 17:54:34 -0500

    task10

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'));
-app.get('/cart/:id([0-9]+)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}`);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the payment system');
 });
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
+
 app.get('/available_payments', (req, res) => {
     const info = {
         payment_methods: {
@@ -15,8 +21,11 @@ app.get('/available_payments', (req, res) => {
     };
     res.status(200).send(info);
 });
+
 app.post('/login', (req, res) => {
     res.send(`Welcome ${req.body.userName}`);
 });
 
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+});
