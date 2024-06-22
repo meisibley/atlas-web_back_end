692dcef1feffe25bfec0e444b62400118f5c4ff4
meisibley <mkong99@yahoo.com>
2024-06-22 15:06:28 -0500

    task5

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,26 +1,29 @@
 const sendPaymentRequestToApi = require('./5-payment');
-const sinon = require('sinon');
 const Utils = require('./utils');
-const chai = require('chai');
+const sinon = require('sinon');
+const { expect } = require('chai');
 
-describe('test sendPaymentRequestToAPI', function () {
-    let consoleSpy;
 
-    beforeEach(function () {
-        consoleSpy = sinon.spy(console, 'log');
-    });
-    afterEach(function () {
-        consoleSpy.restore();
-    });
+describe('Test suite', function () {
+	let consoleSpy;
 
-    it('validate sendPaymentRequestToAPI with value (100, 20)', function () {
-        sendPaymentRequestToApi(100, 20);
-        chai.expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
-        chai.expect(consoleSpy.calledOnce).to.be.true;
-    });
-    it('validate sendPaymentRequestToAPI with value (10, 10)', function () {
-        sendPaymentRequestToApi(10, 10);
-        chai.expect(consoleSpy.calledWithExactly('The total is 20')).to.be.true;
-        chai.expect(consoleSpy.calledOnce).to.be.true;
-    });
-});
+	beforeEach(function () {
+		consoleSpy = sinon.consoleSpy(console, 'log');
+	});
+
+	afterEach(function () {
+		consoleSpy.restore();
+	});
+
+	it('validate response from console.log for value (100, 20)', function () {
+		sendPaymentRequestToApi(100, 20);
+		expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+		expect(consoleSpy.calledOnce).to.be.true;
+	});
+
+	it('validate response from console.log for value (10, 10)', function () {
+		sendPaymentRequestToApi(10, 10);
+		expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+		expect(consoleSpy.calledOnce).to.be.true;
+	});
+})
