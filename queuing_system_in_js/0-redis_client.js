6ecd0a7b88bad1b6163eef04b35a95550fa0c73c
meisibley <mkong99@yahoo.com>
2024-06-25 11:58:12 -0500

    task1 - Node Redis Client

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,8 @@
+import { createClient } from 'redis';
+
+
+const redisClient = createClient();
+redisClient.on('Connected', () =>
+    console.log('Redis client connected to the server'));
+redisClient.on('error', (err) =>
+    console.log(`Redis client not connected to the server: ${err}`));
