ef3721571bcd31f67b4e4a84823e6b2f8931163b
meisibley <mkong99@yahoo.com>
2024-06-25 13:11:56 -0500

    task2 Node redis client and basic operations

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,5 @@
 import { createClient } from 'redis';
 
-
 const redisClient = createClient();
 redisClient.on('Connected', () =>
     console.log('Redis client connected to the server'));
---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+const redis = require('redis');
+redisClient.on('Connected', () =>
+    console.log('Redis client connected to the server'));
+redisClient.on('error', (err) =>
+    console.log(`Redis client not connected to the server: ${err}`));
+
+const setNewSchool = (schoolName, value) => {
+    redisClient.set(schoolName, value, redis.print);
+};
+const displaySchoolValue = (schoolName) => {
+    redisClient.get(schoolName, (err, value) => console.log(value));
+};
+   
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
