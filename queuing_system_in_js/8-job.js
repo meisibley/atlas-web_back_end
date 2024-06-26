47db6e618b234c6557e718742c326410cb318286
meisibley <mkong99@yahoo.com>
2024-06-26 14:31:20 -0500

    task10 - job creation function

--- queuing_system_in_js/8-job.js
+++ queuing_system_in_js/8-job.js
@@ -1,17 +1,18 @@
-const kue = require('kue');
-
 const createPushNotificationsJobs = (jobs, queue) => {
-    if (!(Array.isArray(jobs))) throw (new Error('Jobs is not an array'));
+    if (!(Array.isArray(jobs))) {
+        throw new Error('Jobs is not an array');
+    }
   
-    for (const eachJob of jobs) {
-      let job = queue.create('push_notification_code_3', eachJob)
-        .save(err => {
+    jobs.forEach((eachJob) => {
+      const job = queue.create('push_notification_code_3', eachJob)
+        .save((err) => {
           if (!err) console.log(`Notification job created: ${job.id}`);
         })
         .on('complete', () => console.log(`Notification job ${job.id} completed`))
-        .on('failed', (err) => console.log(`Notification job ${job.id} failed: ${err}`))
+        .on('fail', (err) => console.log(`Notification job ${job.id} failed: ${err}`))
         .on('progress', (percentage) => 
             console.log(`Notification job ${job.id} ${percentage}% complete`));
-    }
+    });
   }
-  export default createPushNotificationsJobs;
+
+  module.exports = createPushNotificationsJobs;
